b55a4a45e42a3d1fa1932f0030bc93635359e905
rhoaiasr <ricardoberrios08@gmail.com>
2024-12-06 11:06:34 -0500

    Add files via upload

---
+++ clase-bot.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+
+token = "MTMxMjA3NTQ0NTQxMTUxNjUwOA.G37uvD.cvmgXefR0-8o7_Ntko4Cjp3Dfzn1jLROQ1_j9I"
+
+bot.run (token)
